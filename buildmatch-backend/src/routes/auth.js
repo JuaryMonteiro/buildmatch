cd4a48381a651a994c0e8fbb280d819b0ad58553
Hugo Monteiro <hmonteiro.l22@us.edu.cv>
2026-07-01 19:42:56 -0100

    corrigir import do PrismaClient

--- buildmatch-backend/src/routes/auth.js
+++ buildmatch-backend/src/routes/auth.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bcrypt  = require('bcrypt');
 const jwt     = require('jsonwebtoken');
-const { PrismaClient } = require('../../generated/prisma');
+const { PrismaClient } = require('@prisma/client');
 const authMiddleware   = require('../middleware/auth');
 
 const router = express.Router();
